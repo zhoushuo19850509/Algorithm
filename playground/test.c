eae9a4da26fd8879c2b431c81a6d654e3d8e2bc5
周硕 <zhoushuo@zhoushuodeMacBook-Air.local>
2025-05-21 22:48:47 +0800

    1.delete useless files ; 2. add playground

--- playground/test.c
+++ playground/test.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+int sum(int N);
+
 int main(){
-    printf("test algorithm ...");
+    printf("test algorithm ...\n");
+    int result = sum(10);
+    printf("result: %d \n", result);
+}
+
+int sum(int N){
+    int sum;
+    int i;
+
+    for(i = 1; i <= N ; i++){
+        sum += i * i * i;
+    }
+
+    return sum;
 }
--- readme.txt
+++ readme.txt
@@ -1,1 +1,6 @@
 this dir is examples of the famous book "Data Structures and Algotithm Analysis in C"
+这里这个word只是归档，最新的文档放在语雀
+
+
+
+
